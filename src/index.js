3deb90cf9cf1558b55eee7640758f670ddf5b0b9
Johnny-Kelvin <johnnykelvin64@gmail.com>
2024-02-29 23:15:48 -0300

    update port

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const  db = require("./database/database");
 const init = require('./app');
 const app = require('./app');
 
-const port = 3000;
+const port = 10000;
 
 
 const start = async () => {
   if (require.main === module) {
     // called directly i.e. "node app"
-    app.listen({ port: 3000 }, (err) => {
+    app.listen({ port: port }, (err) => {
       if (err) console.error(err);
       console.log('server listening on 3000');
     });
